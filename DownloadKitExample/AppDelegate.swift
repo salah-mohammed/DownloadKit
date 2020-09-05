0da8f59168c451367532f2a4cd119325457c3569
Salah <Salahalimohamed1995@gmail.com>
2020-09-05 16:03:12 +0300

    no message

--- DownloadKitExample/AppDelegate.swift
+++ DownloadKitExample/AppDelegate.swift
@@ -10,8 +10,23 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var backgroundCompletionHandler:(() -> Void)?
 
-
+    func application(_ application: UIApplication,
+                     handleEventsForBackgroundURLSession identifier: String,
+                     completionHandler: @escaping () -> Void) {
+            backgroundCompletionHandler = completionHandler
+    }
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        DispatchQueue.main.async {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+                let backgroundCompletionHandler =
+                appDelegate.backgroundCompletionHandler else {
+                    return
+            }
+            backgroundCompletionHandler()
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,7 +46,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+        func applicationDidEnterBackground(_ application: UIApplication) {
+    //         Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    //         If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+            var finished = false
+            var bgTask: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0);
+            bgTask = application.beginBackgroundTask(withName:"MyBackgroundTask", expirationHandler: {() -> Void in
+                // Time is up.
+                if bgTask != UIBackgroundTaskIdentifier.invalid {
+                    // Do something to stop our background task or the app will be killed
+                    finished = true
+                    application.endBackgroundTask(bgTask)
+                    bgTask = UIBackgroundTaskIdentifier.invalid
+                }
+            })
+        }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        var bgTask: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier(rawValue: 0);
+         application.endBackgroundTask(bgTask)
+         bgTask = UIBackgroundTaskIdentifier.invalid
+    }
 }
 
